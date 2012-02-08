8002ee140be13bb6ac188b3500f37f38d9916827
Benjamin Schmidt <bschmidt@chaucer.(none)>
2012-02-08 14:11:15 -0500

    Adding BensPython Functions.

--- Rscripts/script.R
+++ Rscripts/script.R
@@ -1,4 +1,7 @@
 source("Rbindings.R")
+source("Dating tools.R")
+total = dbGetQuery(con,"SELECT words,year FROM presidio.1grams WHERE word1='the'")
+
 html = readLines(url("http://scriptline.livejournal.com/45325.html"))
 downtons = c("http://scriptline.livejournal.com/41950.html",
              "http://scriptline.livejournal.com/42491.html",
@@ -14,72 +17,29 @@ downtons = c("http://scriptline.livejournal.com/41950.html",
 #save(scripts,file = "~/Downtoneps.R")
 load("~/Downtoneps.R")
 downton = scripts
-
 downton = lapply(downton,parseScriptline)
 names(downton) = c(1:length(downton))
 readable = lapply(downton,strsplit,"\n")
 readable = unlist(readable)
-pp1 = readLines(url("http://scriptline.livejournal.com/449.html"))
+
+pandp = c("http://scriptline.livejournal.com/449.html")
+
 #html = readLines(url("http://www.weeklyscript.com/Duck%20Soup.txt"))
 #html = paste(html,collapse = " ")
 #html = gsub("  +"," ",html)
 #script = html
-parseScriptline = function(html) {
-  script = html[grep('entry-content',html)]
-  script = gsub("<[^>]+>","\n",script,perl=T)
-  script = gsub("\\[.*\\]"," ",script,perl=T)
-  #script = gsub("\\n[A-Z ,]+\\n"," ",script,perl=T)
-}
 
-tokenize = function(script,grams=2) {
-  script = gsub("\n",' ',script)
-  script = gsub("([.,!?\\[])"," \\1",script)
-  splitted = strsplit(script," ")
-  splitted = unlist(splitted)
-  splitted = splitted[grep("^$",splitted,perl=T,invert=T)]
-  words = cbind(splitted[-length(splitted)],splitted[-1])
-  words = words[grepl("^\\w+$",words[,1],perl=T) & grepl("^\\w+$",words[,2],perl=T),]
-  words
-}
-  
-total = dbGetQuery(con,"SELECT words,year FROM presidio.1grams WHERE word1='the'")
 
 dcounts = lapply(downton,tokenize)
 dcounts = do.call(rbind,dcounts)
-
-counts = function(dcounts) {
-  counts = table(paste(dcounts[,1],dcounts[,2]))
-  words = as.data.frame(do.call(rbind,strsplit(names(counts)," ")))
-  words$count = counts
-  words = words[!grepl("[B-HJ-Z]",apply(words,1,paste,collapse=" "),perl=T),]
-}
-
+  
 dcounts = counts(dcounts)
-
-modernityCheck = function(row,compareYear=1921,baseyear=2000) {
-  cat(row,"\n")
-  word1 = row[1]
-  word2 = row[2]
-  local = dbGetQuery(con,paste(
-    "SELECT words,year FROM ngrams.2grams WHERE word1='",word1,"' AND word2='",word2,"'",sep=""
-                       ))
-    if(dim(local)[1]) {
-    merged = merge(total,local,by='year',all.x=T)
-    merged$words.y[is.na(merged$words.y)]=0
-    merged$ratio = merged$words.y/merged$words.x
-    require(zoo)
-    merged$smoothed = rollapply(merged$ratio,14,mean,fill=NA)
-    merged = merged[merged$year > 1800,]
-    c(merged$smoothed[merged$year==compareYear],merged$smoothed[merged$year==baseyear])
-    } else {1}
-}
-
-  modernityCheck(c("got","shafted"))
+modernityCheck(c("got","shafted"))
 scores = apply(dcounts,1,modernityCheck,compareYear=1918)
+
 scores = do.call(rbind,scores)
 rownames(scores) = apply(dcounts,1,paste,collapse=" ")
 scores[,1][scores[,1] < 1e-11] = 1e-11
-
 change = data.frame(change = scores[,1]/scores[,2],early=scores[,1],late=scores[,2])
 change = change[change$early!=1,]
 change$freq = cut(change$early,breaks=quantile(change$early),include.lowest=T)
@@ -92,6 +52,35 @@ word = "stole secrets";cat(readable[(grep(word,readable)-7):(grep(word,readable)
 
 readable[grep("cow pie",readable)]
 
- l oc = change[change$late > 1e-7,]; loc =change
+loc = change[change$late > 1e-7,]; loc =change
 loc[order(loc$change),][1:25,]
 change[change$early > 1e-06,]
+
+
+if (FALSE) {
+  wharton = scan("~/wharton.txt",what='raw')
+  wharton = paste(script,collapse="\n")
+  tok = tokenize(wharton)
+  source("Dating tools.R")
+  cnts = counts(tok)
+  cnts[1:5,]
+  scores = apply(cnts,1,modernityCheck,compareYear=1875,baseyear=1921)
+  scores = do.call(rbind,scores)
+  rownames(scores) = apply(cnts[,1:2],1,paste,collapse=" ")
+  scores[,1][scores[,1] < 1e-15] = 1e-15
+  change = data.frame(change = scores[,1]/scores[,2],early=scores[,1],late=scores[,2])
+  change = change[change$early!=1,]
+  change = change[change$late!=1,]
+  change$isNew = change$change<1
+  change$abslog = abs(log(change$change))
+  qplot(change$abslog)
+  change$freq = cut(change$early,breaks=quantile(change$early),include.lowest=T)
+  ggplot(change[change$change > 0 & cnts[,3] > 1,],aes(x=change)) + 
+    geom_histogram(binwidth=.1) + scale_x_continuous(trans='log10',limits = c(.01,100)) +
+    facet_wrap(~freq) + scale_y_sqrt()
+  ggplot(change[change$abslog<10,],aes(x=abslog,fill=isNew)) + geom_density(alpha=.5)
+  f = lm(log(early) ~ log(late),change[change$early > 0 & change$late > 0,])
+  sort(f$residuals)[1:55]
+  word = "stole secrets";cat(readable[(grep(word,readable)-7):(grep(word,readable)+7)],sep="\n")
+}
+
