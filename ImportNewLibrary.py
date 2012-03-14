836d5d7caa91ec6342275ec3c2e41b399d7b7278
bmschmidt <bschmitd@bookworm-database.(none)>
2012-03-14 04:11:18 +0000

    Adding a few more thing

--- ImportNewLibrary.py
+++ ImportNewLibrary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#Currently only doing the last few stages!
+
 import subprocess
 from subprocess import call as sh
 import os
@@ -32,11 +32,11 @@ for directory in ['texts','logs','texts/cleaned','logs','logs/clean','texts/unig
 
 """Use the cleaning program to make texts that are set for tokenizing, and with sentences at linebreaks."""
 print "Cleaning the texts"
-#sh(['python','master.py','clean'])
+sh(['python','master.py','clean'])
 print "Creating 1 gram counts"
-#sh(['python','master.py','unigrams'])
+sh(['python','master.py','unigrams'])
 print "Creating 2gram counts"
-#sh(['python','master.py','bigrams'])
+sh(['python','master.py','bigrams'])
 #We could add 3grams, and so forth, here.
 
 print "Creating a master wordlist"
