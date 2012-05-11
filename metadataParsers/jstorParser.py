86d9a7186debbf7cdb7d09a4737bc13fa1f44105
Ben Schmidt <bmschmidt@gmail.com>
2012-05-11 11:39:42 +0000

    jstor edits 2

--- metadataParsers/jstorParser.py
+++ metadataParsers/jstorParser.py
@@ -3,9 +3,11 @@
 import re
 import codecs
 from xml.dom.minidom import parse,parseString
+import json
 
 metadata_fields = ['year','month','day','journalabbrv','journalid','headid','title','languages','type','journaltitle','volume','authors','issueid','issn','fpage','lpage']
 
+metadatafile = open ('../../metadata/jsoncatalog.txt','a')
 
 class Export:
     def __init__(self):
@@ -17,41 +19,42 @@ class Export:
             self.filenames.append(mystring)
     def exportall(self):
         for p in self.filenames:
-            working = jstorID(p)
+            working = jstorID(p,metadatafile)
             #working.writeText()
             working.writeMetadata()
             
-class jstorID:
-    def __init__(self,string):
+class jstorID():
+    def __init__(self,string,metadatafile):
         self.id = string
+        self.metadatafile=metadatafile
         self.open_xml()
         self.get_metadata()
     def open_xml(self):
         self.dom = parse('../../raw/' + self.id + '.xml')
         self.text = self.dom.getElementsByTagName('text')
         self.textstring = "\n".join([t.childNodes[0].nodeValue for t in self.text])
-        self.textstring = self.textstring.encode('utf-8')
+        #self.textstring = self.textstring.encode('utf-8')
     def get_metadata(self):
         self.metadata = dict()
         for item in metadata_fields:
             try:
                 self.metadata[item] = self.dom.getElementsByTagName(item)[0].childNodes[0].nodeValue
-                self.metadata[item] = self.metadata[item].encode('utf-8')
+                #self.metadata[item] = self.metadata[item].encode('utf-8')
+                self.metadata[item] = re.sub("\n$","",self.metadata[item])
+                self.metadata[item] = re.sub(" +$","",self.metadata[item])
+                self.metadata[item] = re.sub("\n","",self.metadata[item])
+                self.metadata[item] = re.sub("\t","",self.metadata[item])
             except:
-                self.metadata[item] = ''
+                self.metadata[item] = ""
     def writeText(self):
         output = open('../../texts/' + self.id + '.txt','w')
         output.write(self.textstring)
         output.close()
     def writeMetadata(self):
         self.get_metadata()
-        metadata = open ('../../metadata/main.txt','a')
-        metadata.write(self.id)
-        for field in metadata_fields:
-            try:
-                metadata.write(self.metadata[field] + "\t")
-            except KeyError:
-                metadata.write("\t")
-        metadata.write("\n")
+        self.metadata["id"] = str(self.id)
+        self.metadata["searchstring"] = self.metadata["title"] + "," + self.metadata["journaltitle"] + "<a href=www.jstor.org/" + re.sub("_","/",self.id) + ">" + read + "</a>"
+        self.metadatafile.write(json.dumps(self.metadata))
+        self.metadatafile.write("\n")
 
 Export().exportall()
