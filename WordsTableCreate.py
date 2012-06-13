38793e678f96e7f80ccb93ea2d9f03034ce72ee5
Benjamin Schmidt <bschmidt@chaucer.(none)>
2012-06-13 01:01:24 -0400

    Fix duplicate error in words tables (caused by variable collation fields)

--- WordsTableCreate.py
+++ WordsTableCreate.py
@@ -5,21 +5,20 @@ import sys
 import re
 import subprocess
 from subprocess import call
+
 maxDictionaryLength=2000000
 #10 million words should be about 1 gigabyte of memory, which is plenty on the servers we use: flush out every time we hit that limit.
-maxMemoryStorage = 40000000
+maxMemoryStorage = 10000000
 wordcounts = dict()
 filenum = 1
 readnum = 0
 logfile = open("log.log",'w')
+database = open("../texts/wordlist/raw.txt",'w')
 
-try:
-    call(['mkdir','../texts/wordlist/tmp'])
-except:
-    print "Error on mkdir: is it already there?"
 
 for file in os.listdir('../texts/textids'):
     for line in open('../texts/textids/' + file,'r'):
+        filenum = filenum + 1
         filename = line.split('\t')[1]
         filename = re.sub('\n','',filename)
         try:
@@ -41,58 +40,39 @@ for file in os.listdir('../texts/textids'):
 #Now we need to delete the words that appear below a cutoff that we find dynamically:
                 countcounts = dict()
         except:
-            print "Error on WordsTableCreate: could not find file " + filename + " (...moving on to next file...)"
+            pass
+            #print "Error on WordsTableCreate: could not find file " + filename + " (...moving on to next file...)"
         if len(wordcounts) > maxMemoryStorage:
-            logfile.write("Creating tmp table number " + str(filenum) + " (" + str(readnum) + "books)\n")
-            file = open('../texts/wordlist/tmp/'+str(filenum) + '.txt','w')
-            filenum=filenum+1
+            print "exporting to disk at file number " + str(filenum)
             keys = wordcounts.keys()
-            keys.sort()
             for key in keys:
-                file.write(key+"\t"+str(wordcounts[key])+"\n")
+                database.write(key + " " + str(wordcounts[key])+"\n")
             wordcounts=dict()
+            print "  export done"
+#        if filenum > 1000:
+#            break
 
-#Then use textutils to do all the hard work without having to worry about memory management.
+database.close()
 
-#Do it again on the leftovers.
-file = open('../texts/wordlist/tmp/'+str(filenum) + '.txt','w')
-keys = wordcounts.keys()
-keys.sort()
-for key in keys:
-    file.write(key+"\t"+str(wordcounts[key])+"\n")
+print("Sorting full word counts\n")
+#This LC_COLLATE here seems to be extremely necessary, unfortunately for sanity's sake.
+call(["export LC_COLLATE='C'; sort -k1 ../texts/wordlist/raw.txt > ../texts/wordlist/sorted.txt"],shell=True)
+print("Collapsing word counts\n")
+call(["""awk 'BEGIN{count=0}
+        {
+         if (NR==1)
+            {count=0;word=$1;}
+         if ($1==word)
+            count += $2
+         if ($1!=word) {
+            print word " " count;
+            word=$1;
+            count=$2;
+            }
+        } END {print word " " count;}
+        ' ../texts/wordlist/sorted.txt > ../texts/wordlist/counts.txt"""],shell=True) 
 
-logfile.write("merging tmpfiles\n")
 
-if filenum == 1:
-    args = ["cat","../texts/wordlist/tmp/1.txt"]
-
-if filenum > 1:
-    args = ["join","-a1","-a2","../texts/wordlist/tmp/1.txt","../texts/wordlist/tmp/2.txt"]
-
-if filenum > 2:
-    for i in range(3,filenum+1):
-    #Each join is piped to the next file in the chain to do them all at once:
-        for string in ["|","join","-a1","-a2","-","../texts/wordlist/tmp/" + str(i)+".txt"]:
-            args.append(string)
-
-args.append(">")
-args.append("../texts/wordlist/tmp/countnums.txt")
-logfile.write( "Running code snippet of -- " + " ".join(args))
-
-call(" ".join(args),shell=True)
-
-logfile.write("tmpfiles merged; getting full word counts\n")
-
-call(["""
-awk '
-BEGIN {FS=" ";OFS="\t"}
-{
-sum=0;
-for(i=2;i<=NF;i++)
-     {sum+=$i}
-     print $1 "\t" sum}' ../texts/wordlist/tmp/countnums.txt > ../texts/wordlist/counts.txt"""],shell=True) 
-
-logfile.write("Sorting full word counts\n")
 call(["""sort -nrk2 ../texts/wordlist/counts.txt > ../texts/wordlist/complete.txt"""],shell=True)
 logfile.write("Adding rank numbers to the usable ones")
 call(["""head -""" + str(maxDictionaryLength) + """ ../texts/wordlist/complete.txt | awk '{print NR "\t" $0}' > ../texts/wordlist/wordlist.txt """],shell=True)  
