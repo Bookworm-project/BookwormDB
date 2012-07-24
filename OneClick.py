8f8fcbe5897cd4856cf97c119bb48cc1d5224afd
Ben Schmidt <bmschmidt@gmail.com>
2012-07-24 14:50:35 +0000

    ngrams additions

--- OneClick.py
+++ OneClick.py
@@ -9,21 +9,24 @@ from subprocess import call
 #First off: what are we using? Pull a dbname from command line input.                                                                 
 #As well as the username and password. 
 dbname = sys.argv[1]
-call(['python','ParseDate.py',dbname])
+
+print "Parsing the dates to a native format"
+#call(['python','ParseDate.py',dbname])
 
 
+"Loading necessary scripts..."
 execfile("CreateDatabase.py")
 
+"Writing metadata to new catalog file..."
+#write_metadata()
 
-write_metadata()
-
-#execfile("ImportNewLibrary.py")
+execfile("ImportNewLibrary.py")
 
 #Most of these commands are inside CreateNewDatabase.py
-#load_word_list()
-#create_unigram_book_counts()
-#create_bigram_book_counts()
-#load_book_list()
-#create_memory_table_script(allVariables)
-#jsonify_data(allVariables)
-#create_API_settings(allVariables)
+load_word_list()
+create_unigram_book_counts()
+create_bigram_book_counts()
+load_book_list()
+create_memory_table_script(allVariables)
+jsonify_data(allVariables)
+create_API_settings(allVariables)
