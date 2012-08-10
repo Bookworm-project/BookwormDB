53616842323bd316d6649abbbdf439eefef13efb
billyjanitsch <janitsch@college.harvard.edu>
2012-08-10 16:37:16 -0400

    New date format

--- ParseDate.py
+++ ParseDate.py
@@ -5,9 +5,7 @@ import sys
 
 PROJECT = sys.argv[1]
 
-f = open("../metadata/jsoncatalog.txt", "r")
-md = f.readlines()
-f.close()
+
 
 f = open("../metadata/field_descriptions.json", "r")
 fields = json.loads(f.read())
@@ -28,25 +26,32 @@ for field in fields:
 
 for field in fields_to_derive:
     for derive in field["derived"]:
-        output.append({"field":str(json.dumps(field["field"])[1:-1] + '_' + json.dumps(derive)[1:-1]), "datatype":"time", "type":"integer", "unique":True})
-f.write(json.dumps(output).replace("[", "[\n").replace("}, {", "},\n{").replace("]", "\n]"))
+        if "aggregate" in derive: output.append({"field":'_'.join([field["field"], derive["resolution"], derive["aggregate"]]), "datatype":"time", "type":"integer", "unique":True})
+        else: output.append({"field":'_'.join([field["field"], derive["resolution"]]), "datatype":"time", "type":"integer", "unique":True})
+f.write(json.dumps(output))
 f.close()
 
+
+f = open("../metadata/jsoncatalog.txt", "r")
+md = f.readlines()
+f.close()
+
+order = ["year", "month", "week", "day"]
 
 f = open("../metadata/jsoncatalog_derived.txt", "w")
 for data in md:
     line = json.loads(data)
     for field in fields_to_derive:
         content = line[field["field"]].split('-')
-        if len(content) in [0,1]: continue
+        if len(content) == 0: continue
         else:
             to_derive = field["derived"]
             for derive in to_derive:
                 l = derive.split("_")
-                if len(l) == 1:
+                if len(l) == 2:
                     if l[0] == "year": line[field["field"] + "_year"] = content[0]
                     else: continue
-                else:
+                else if len(l) == 3:
                     if l[0] == "month":
                         if l[1] == "round": line[field["field"] + "_month_round"] = (datetime.datetime.strptime('%02d'%int(content[1])+content[0], "%m%Y").date() - datetime.date(1,1,1)).days
                         elif l[1] == "cycle": line[field["field"] + "_month_cycle"] = content[1]
