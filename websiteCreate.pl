ee1634512be994f32705cd157de419d4012ab690
Ben Schmidt <bmschmidt@gmail.com>
2012-11-15 19:18:50 +0000

    Deleting some old files

--- websiteCreate.pl
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/perl
-
-$database = shift @ARGV or die "Must specify dbname";
-
-
-`cp -r /var/www/OL /var/www/$database`;
-`cp -r ../$database.json /var/www/$database/static/$database.json`;
-`cp -r /var/www/$database/default.html /var/www/$database/$database.html`;
-`cd /var/www/$database; perl setVersion.pl $database`;
