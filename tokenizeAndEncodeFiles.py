da59162b2a997517b2e282d09beb1edb03242ecf
Ben Schmidt <root@ip-10-98-27-130.ec2.internal>
2013-02-05 16:47:39 -0500

    fixed stupid processor mistake

--- tokenizeAndEncodeFiles.py
+++ tokenizeAndEncodeFiles.py
@@ -34,10 +34,10 @@ class bookidlist:
         self.bookids = bookids
 
         print "Done reading in files: moving to create subprocesses"
-        self.processors = multiprocessing.cpu_count()*2 #Try double for some Intel processors: can't hurt much, right?
+        self.processors = multiprocessing.cpu_count() #Try double for some Intel processors: can't hurt much, right?
         #We can accept at most 100K arguments at a time on the command line.
         #but varies from system to system. So break up the chunks into lots and lots of pieces.
-        self.processes = min([max([self.processors,int(len(self.bookids)/10000)]),8])
+        self.processes = max([self.processors,int(len(self.bookids)/10000)])
         #and make it divisible by the number of processors which should help make it go optimally fast.
         #(although it should maybe be down to n-1 or something, in case one thread is hanging or one is super-speedy)
         self.processes = int(self.processes/self.processors)*self.processors + self.processors
