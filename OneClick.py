a5b89743f802093a87ddec2b9fccc0ad22c58eca
econpy <matt@econpy.org>
2013-03-17 23:05:13 -0400

    Modularized ParseDate.py functions into OneClick.py

--- OneClick.py
+++ OneClick.py
@@ -5,10 +5,13 @@ import json
 import os
 from subprocess import call
 
+from ParseDate import DateParser
+
 #These three libraries define the Bookworm-specific methods.
 from CreateDatabase import *
 from ImportNewLibrary import *
 from WordsTableCreate import WordsTableCreate
+from tokenizeAndEncodeFiles import bookidlist
 
 #First off: what are we using? Pull a dbname from command line input.                                                                 
 #As well as the username and password. 
@@ -17,36 +20,38 @@ dbuser = sys.argv[2]
 dbpassword = sys.argv[3]
 
 print "Parsing the dates to a native format"
-#This should be brought into the modular fold.
-call(['python','ParseDate.py',dbname])
+DateParser()
 
 Bookworm = BookwormSQLDatabase(dbname,dbuser,dbpassword)
 
-
-
-"Writing metadata to new catalog file..."
+print "Writing metadata to new catalog file..."
 
 write_metadata(Bookworm.variables)
 
 #These are imported with ImportNewLibrary
+#Some sort of check here might make things a little faster on subsequent builds.
 CopyDirectoryStructuresFromRawDirectory()
 
-CleanTexts()
-MakeUnigramCounts()
-MakeBigramCounts()
-MakeTrigramCounts() #Doesn't do nothing yet.
+bookidList = bookidlist()
+
+bookidList.clean()
+bookidList.tokenize('unigrams')
+bookidList.tokenize('bigrams')
+
+#bookidList.tokenize('trigrams')
 
 print "Creating a master wordlist" #These values shouldn't be hard-coded in, probably:
 WordsTableCreate(maxDictionaryLength=1000000,maxMemoryStorage = 15000000)
 
-EncodeUnigrams()
-EncodeBigrams()
+bookidList.encodeUnigrams()
+bookidList.encodeBigrams()
+#bookidList.encodeTrigrams()
 
 
 """
 Most of these commands are inside CreateNewDatabase.py. For manual constructions or 
 stop-and-start operations, it's very helpful to have them spread out like this.
-The class initialization should be quick, but does create a database if it's not there.
+The class initialization should be quick, but--fair warning--does create a database if it's not there.
 """
 
 Bookworm.load_word_list()
