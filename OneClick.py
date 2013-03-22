933e4564ee629c6dfe401a0177ae462ae6e7ac76
Matt <matthew.nicklay@gmail.com>
2013-03-22 00:58:16 -0300

    Improving upon comments.

--- OneClick.py
+++ OneClick.py
@@ -5,59 +5,54 @@ import json
 import os
 from subprocess import call
 
-from ParseDate import DateParser
-
-#These three libraries define the Bookworm-specific methods.
+# These three libraries define the Bookworm-specific methods.
 from CreateDatabase import *
 from ImportNewLibrary import *
 from WordsTableCreate import WordsTableCreate
 from tokenizeAndEncodeFiles import bookidlist
 
-#First off: what are we using? Pull a dbname from command line input.                                                                 
-#As well as the username and password. 
+# Import our data parser.
+import ParseDate
+
+# Pull a dbname from command line input.
 dbname = sys.argv[1]
 dbuser = sys.argv[2]
 dbpassword = sys.argv[3]
 
 print "Parsing the dates to a native format"
-DateParser()
+ParseDate.DateParser()
 
+# Initiate MySQL connection.
 Bookworm = BookwormSQLDatabase(dbname,dbuser,dbpassword)
 
+# This creates helper files in the /metadata/ folder.
 print "Writing metadata to new catalog file..."
-
 write_metadata(Bookworm.variables)
 
 #These are imported with ImportNewLibrary
-#Some sort of check here might make things a little faster on subsequent builds.
-CopyDirectoryStructuresFromRawDirectory()
-
+CopyDirectoryStructuresFromRawDirectory() # rsync is very slow when the number of files in /texts/raw/ is large (1000000+)
 bookidList = bookidlist()
-
 bookidList.clean()
 bookidList.tokenize('unigrams')
 bookidList.tokenize('bigrams')
-
 #bookidList.tokenize('trigrams')
-
-print "Creating a master wordlist" #These values shouldn't be hard-coded in, probably:
+print "Creating a master wordlist"
 WordsTableCreate(maxDictionaryLength=1000000,maxMemoryStorage = 15000000)
-
 bookidList.encodeUnigrams()
 bookidList.encodeBigrams()
 #bookidList.encodeTrigrams()
 
 
-"""
-Most of these commands are inside CreateNewDatabase.py. For manual constructions or 
-stop-and-start operations, it's very helpful to have them spread out like this.
-The class initialization should be quick, but--fair warning--does create a database if it's not there.
-"""
-
+# Most of these commands are inside /Presidio/CreateNewDatabase.py.
 Bookworm.load_word_list()
 Bookworm.create_unigram_book_counts()
 Bookworm.create_bigram_book_counts()
 Bookworm.load_book_list()
+
+# This needs to be run if the database resets. It builds a temporary MySQL table and the GUI will not work if this table not built.
 Bookworm.create_memory_table_script()
+
+# Create the dbname.json file in the root directory. Move this to the $project/dbname/static/ folder of the web server.
 Bookworm.jsonify_data()
+
 Bookworm.create_API_settings()
