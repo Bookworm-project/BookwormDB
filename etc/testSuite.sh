88e7b4f23338d0dc61569effa64284a0b73c177c
Ben Schmidt <bmschmidt@gmail.com>
2013-04-09 15:48:34 -0400

    bringing testSuite up to date

--- etc/testSuite.sh
+++ etc/testSuite.sh
@@ -3,13 +3,13 @@
 
 #just clone the whole guy in there--probably a better way to do this.
 #The dot keeps it from copying into itself.
-mkdir .presidio
-cp -r * .presidio
-
-
 
 curl -o HistoryDiss.tar.gz chaucer.fas.harvard.edu/HistoryDiss.tar.gz
 tar -zxf HistoryDiss.tar.gz
+rm HistoryDiss.tar.gz
+
+mkdir files
+mv files oldfiles
 mv .presidio HistoryDiss/presidio
 
 cd HistoryDiss/presidio
@@ -18,12 +18,10 @@ cd HistoryDiss/presidio
 echo "DROP DATABASE IF EXISTS HistoryDissTest;" | mysql mysql
 python OneClick.py HistoryDissTest test password > results.txt
 
-#cd ..
-#rm -r HistoryDiss
 echo "Delete the files (you should only keep to check for errors)"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) rm -r HistoryDiss; break;;
+        Yes ) rm -r files; mv oldfiles files; break;;
         No ) exit;;
     esac
 done
