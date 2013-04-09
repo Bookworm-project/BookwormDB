18851efb0c8d90d2717ac89b6ed0441531b166e1
Ben Schmidt <bmschmidt@gmail.com>
2013-04-09 16:05:59 -0400

    further fixes to testSuite.sh

--- etc/testSuite.sh
+++ etc/testSuite.sh
@@ -1,8 +1,7 @@
 
 #!/usr/bin/sh
 
-#just clone the whole guy in there--probably a better way to do this.
-#The dot keeps it from copying into itself.
+#Download a test suite: by default, it's this address here.
 
 curl -o HistoryDiss.tar.gz chaucer.fas.harvard.edu/HistoryDiss.tar.gz
 tar -zxf HistoryDiss.tar.gz
@@ -10,13 +9,11 @@ rm HistoryDiss.tar.gz
 
 mkdir files
 mv files oldfiles
-mv .presidio HistoryDiss/presidio
-
-cd HistoryDiss/presidio
+mv HistoryDiss files
 
 #This will drop any databases called "historyDissTest"
 echo "DROP DATABASE IF EXISTS HistoryDissTest;" | mysql mysql
-python OneClick.py HistoryDissTest test password > results.txt
+python OneClick.py HistoryDissTest test password
 
 echo "Delete the files (you should only keep to check for errors)"
 select yn in "Yes" "No"; do
