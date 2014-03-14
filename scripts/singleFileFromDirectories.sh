30bdfa20db2723a3e6c281f4dd799c38a201bad9
Benjamin Schmidt <bmschmidt@gmail.com>
2014-03-13 22:40:43 -0400

    script to enable conversion of old file types

---
+++ scripts/singleFileFromDirectories.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#This is super-simple; it just takes a filename and converts it into the format that the line-style slurper likes.
+
+filename=$1
+filetext=$(cat files/texts/raw/$filename.txt | sed 's/[\n\r]/ /g')
+echo "$filename	$filetext" 
