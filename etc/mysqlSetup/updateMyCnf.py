06e3262bfea7b15fe0f0b931a25e8dbfa0c1119d
Benjamin Schmidt <bmschmidt@gmail.com>
2014-10-13 20:11:17 -0400

    adding caches to the configuration updater

--- etc/mysqlSetup/updateMyCnf.py
+++ etc/mysqlSetup/updateMyCnf.py
@@ -14,7 +14,7 @@ if not my.read([systemFile]):
 if not my.has_section("mysqld"):
     my.add_section("mysqld")
 
-mysqldoptions = {"max_allowed_packet":"512M","sort_buffer_size":"8M","read_buffer_size":"4M","read_rnd_buffer_size":"8M","bulk_insert_buffer_size":"512M","myisam_sort_buffer_size":"512M","myisam_max_sort_file_size":"1500G","key_buffer_size":"1500M","query_cache_size":"32M","tmp_table_size":"1024M","max_heap_table_size":"1024M","character_set_server":"utf8"}
+mysqldoptions = {"max_allowed_packet":"512M","sort_buffer_size":"8M","read_buffer_size":"4M","read_rnd_buffer_size":"8M","bulk_insert_buffer_size":"512M","myisam_sort_buffer_size":"512M","myisam_max_sort_file_size":"1500G","key_buffer_size":"1500M","query_cache_size":"32M","tmp_table_size":"1024M","max_heap_table_size":"1024M","character_set_server":"utf8","query_cache_type":"1","query_cache_limit":"2M"}
 
 for option in mysqldoptions.keys():
     if not my.has_option("mysqld",option):
