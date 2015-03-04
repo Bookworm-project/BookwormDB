47c1f429cd16ce45dc20a9b33d7d3b501881c1e4
Ben Schmidt <bmschmidt@gmail.com>
2015-03-04 11:24:27 -0500

    look for input files in parent directory

--- scripts/justPrintInputTxt.sh
+++ scripts/justPrintInputTxt.sh
@@ -1,9 +1,18 @@
 #!/bin/sh
 
-#test for input.txt; if it doesn't exist, use every file in /files/texts/raw as an input per the old input format.
+#test for input.txt; if it doesn't exist, check if there's one the level just above: finally, use every file in /files/texts/raw as an input per the old input format.
 
 if [ -f files/texts/input.txt ]; then
-   cat files/texts/input.txt
+    echo "Using stored input.txt file" 1>&2
+    cat files/texts/input.txt
+
 else
-    find -L files/texts/raw -name "*.txt" | perl -pe 's/.*files\/texts\/raw\/(.*).txt/$1/g' | xargs -I filename bash scripts/singleFileFromDirectories.sh filename
+
+    if [ -f ../input.txt ]; then
+	echo "Reading input.txt from single file at the directory above this" 1>&2
+	cat ../input.txt
+    else
+	echo "Reading in a file at a time from files/texts/raw: note that this method may be slow on more than a few hundred thousand texts" 1>&2
+        find -L files/texts/raw -name "*.txt" | perl -pe 's/.*files\/texts\/raw\/(.*).txt/$1/g' | xargs -I filename bash scripts/singleFileFromDirectories.sh filename
+    fi
 fi
