fb92d5703d5da410f602e8e4a61cfe4909b3bba5
Peter Organisciak <organisciak@gmail.com>
2015-03-20 00:28:38 -0500

    Relative directory for feature counting script

--- scripts/fast_featurecounter.sh
+++ scripts/fast_featurecounter.sh
@@ -8,18 +8,19 @@ infile=$1
 # Explicitly set tmp directory to better manage disk needs
 tmpdir=$2
 blocksize=$3
-outfile=files/texts/wordlist/wordlist.txt
+outfile=$4
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
 mkdir -p $tmpdir
 cat $infile |\
 parallel --block $blocksize -j95% --pipe --files --tempdir $tmpdir \
-	'awk {print\ \$2\,\ \$3} | sort | awk -f scripts/mergecounted.awk' >tmp1files.txt
+	awk '{print\ \$2\,\ \$3}' "|" sort "|" awk -f $DIR/mergecounted.awk >tmp1files.txt
 
 # We've processed the files in a big batch, but in all likelihood, there's still too many
 # of them to glob all together and sort. So, let's merge in batches of 30 and dedupe again
-cat tmp1files.txt | parallel --files --tempdir $tmpdir  -Xn30 \
-	"sort -m {} | awk -f scripts/mergecounted.awk; rm {} " |\
-	parallel -Xj1 "sort -m {} | awk -f scripts/mergecounted.awk ; rm {}" |\
+cat tmp1files.txt | parallel --files --tempdir $tmpdir -Xn30 -j95% \
+	sort -m {} "|" awk -f scripts/mergecounted.awk ";" rm {} |\
+	parallel -Xj1 sort -m {} "|" awk -f $DIR/mergecounted.awk ";" rm {} |\
 	sort -n -r -k2 | awk 'BEGIN {i=0}{i+=1;print i "	" $1 "	" $2}' >$outfile # Format for bw
 
 rm tmp1files.txt
