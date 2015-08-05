5a067da8078e5a120b57dfec5aa1983629a21272
Benjamin Schmidt <bmschmidt@gmail.com>
2015-08-04 22:34:19 -0400

    test suite

--- test_bookworm/testAPI.py
+++ test_bookworm/testAPI.py
@@ -1,33 +1,66 @@
-import dbbindings
 import unittest
-import bookworm.general_API as general_API
-import bookworm.SQLAPI as SQLAPI
+import bookwormDB
 
-class SQLfunction(unittest.TestCase):
+class Bookworm_SQL_Creation(unittest.TestCase):
 
-    def test1(self):
+    def test_server_connection(self):
+        """
+        Connect to MySQL and run a simple query.
+        """
+        import bookwormDB.CreateDatabase
+        db = bookwormDB.CreateDatabase.DB(dbname="mysql")
+        sampleQuery=db.query("SELECT 1+1").fetchall()
+        self.assertTrue(sampleQuery[0][0]==2)
 
-        query = {
-            "database": "movies",
-            "method": "return_json",
-            "search_limits": {"MovieYear":1900},
-            "counttype": "WordCount",
-            "groups": ["TV_show"]
-        }
+        
+    def test_bookworm_creation(self):
+        """
+        Creates a test bookworm. Removes any existing databases called "federalist_bookworm"
+        """
+        
+        import MySQLdb
+        from warnings import filterwarnings
+        filterwarnings('ignore', category = MySQLdb.Warning)
+        
+        import bookwormDB.CreateDatabase
+        db = bookwormDB.CreateDatabase.DB(dbname="mysql")
+        if len(db.query("SHOW DATABASES LIKE 'federalist_bookworm'").fetchall()) > 0:
+            db.query("DROP DATABASE federalist_bookworm")
 
-    
-        f = SQLAPI.userquery(query).query()
-        print f
-    
+        from subprocess import call as call
 
-class SQLConnections(unittest.TestCase):
-    def dbConnectorsWork(self):
-        from general_API import prefs as prefs
-        connection = general_API.dbConnect(prefs,"federalist")
-        tables = connection.cursor.execute("SHOW TABLES")
-        self.assertTrue(connection.dbname=="federalist")
+        from urllib2 import urlopen, URLError, HTTPError
 
-    def test1(self):
+        url = "https://github.com/bmschmidt/federalist-bookworm/archive/master.zip"
+        f = urlopen(url)
+        with open("/tmp/federalist.zip", "wb") as local_file:
+            local_file.write(f.read())
+
+        import zipfile  
+        import os
+        import shutil
+        
+        if os.path.exists("/tmp/federalist/federalist-bookworm-master/"):
+            if os.path.exists("/tmp/federalist/federalist-bookworm-master/.bookworm"):
+                shutil.rmtree("/tmp/federalist/federalist-bookworm-master/.bookworm")
+        else:
+            zip = zipfile.ZipFile(r'/tmp/federalist.zip')  
+            zip.extractall(r'/tmp/federalist')
+
+        with open("/tmp/federalist/federalist-bookworm-master/bookworm.cnf","w") as output:
+            output.write("""[client]\ndatabase = federalist_bookworm\nuser=\npassword=\n""")
+            # This doesn't worry about client-side passwords.
+
+        call(["make"],shell=True,cwd="/tmp/federalist/federalist-bookworm-master")
+        
+        db.query("USE federalist_bookworm")
+        wordCount = db.query("SELECT SUM(nwords) FROM fastcat").fetchall()[0][0]
+        self.assertTrue(wordCount>100000)
+        
+    def test_api(self):
+        from bookwormDB.general_API import SQLAPIcall as SQLAPIcall
+        import json
+        
         query = {
                 "database":"federalist",
                 "search_limits":{},
@@ -36,15 +69,15 @@ class SQLConnections(unittest.TestCase):
                 "method":"return_json"
         }
         
-        try:
-            dbbindings.main(query)
-            worked = True
-        except:
-            worked = False
+        m = json.loads(SQLAPIcall(query).execute())
+        self.assertTrue(len(m)==5)
 
-        self.assertTrue(worked)
+        
+class SQLConnections(unittest.TestCase):
+    
+        
 
-    def test2(self):
+    def test_dunning(self):
         query = {
             "database":"federalist",
             "search_limits":{"author":"Hamilton"},
