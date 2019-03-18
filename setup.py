a915a28af970b276efd53bcd45904544a48b1985
Ben Schmidt <bmschmidt@gmail.com>
2019-03-17 23:12:21 -0400

    bounter import

--- setup.py
+++ setup.py
@@ -4,31 +4,33 @@ from setuptools import setup
 
 def figure_out_cgi_directory():
     """
-    Try to place the cgi-scripts in a cgi dir; if that fails, bomb out to the current directory.
+    Try to place the cgi-scripts in a cgi dir;
+    if that fails, bomb out to the current directory.
     """
     for dir in ["/usr/lib/cgi-bin","/Library/WebServer/CGI-Executables/","/var/www/cgi-bin","/tmp","."]:
         if os.path.exists(dir):
             if os.access(dir, os.R_OK):
                 return dir
-            
+
+
 setup(
     name='bookwormDB',
-      packages=["bookwormDB"],
-      version='0.5',
-      entry_points={
-            'console_scripts': [
-                'bookworm = bookwormDB.manager:run_arguments'
-            ],
-      },
-#      data_files = [(figure_out_cgi_directory(), ["bookwormDB/bin/dbbindings.py"])],
-      description="Create, deploy, and serve a Bookworm instance.",
-      long_description = "\n".join(open("README.rst").readlines()),
-      package_data={'bookwormDB':['etc/*','bin/*']},
-      url="http://github.com/Bookworm-Project",
-      author="Benjamin Schmidt",
-      author_email="bmschmidt@gmail.com",
-      license="MIT",
-      classifiers=[
+    packages=["bookwormDB"],
+    version='0.5',
+    entry_points={
+        'console_scripts': [
+            'bookworm = bookwormDB.manager:run_arguments'
+        ],
+    },
+    #      data_files = [(figure_out_cgi_directory(), ["bookwormDB/bin/dbbindings.py"])],
+    description="Create, deploy, and serve a Bookworm instance.",
+    long_description="\n".join(open("README.rst").readlines()),
+    package_data={'bookwormDB':['etc/*','bin/*']},
+    url="http://github.com/Bookworm-Project",
+    author="Benjamin Schmidt",
+    author_email="bmschmidt@gmail.com",
+    license="MIT",
+    classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
         'Intended Audience :: Education',
@@ -43,6 +45,7 @@ setup(
         "Topic :: Sociology :: History",
         "Topic :: Text Processing :: Indexing",
         "Topic :: Text Processing :: Linguistic"
-        ],
-      install_requires = ["numpy","regex","nltk","pandas","mysqlclient","python-dateutil","ujson", "psutil"]
+    ],
+    install_requires=["numpy","regex","nltk","pandas","mysqlclient",
+                      "python-dateutil","ujson", "psutil", "bounter"]
 )
